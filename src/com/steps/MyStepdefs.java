03838591848f192ede226d3bb293ab685af0b392
Virag@EPAM <virag_kumar@epam.com>
2019-01-24 20:33:31 +0530

    cucumber changes

---
+++ src/com/steps/MyStepdefs.java
@@ -0,0 +1,19 @@
+package com.steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MyStepdefs {
+    @Given("I have opened google home page")
+    public void iOpen() {
+    }
+
+    @When("I search a text")
+    public void iSearchAText() {
+    }
+
+    @Then("I validate the outcomes")
+    public void iValidate() {
+    }
+}
